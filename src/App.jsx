25487a71d144f877b1f03c60ee1d0c140594bf2f
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-07 21:15:11 +0530

    daisyUi and Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 import BudgetRouters from './CustomRoutes/BudgetRouters'
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <BudgetRouters/>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
         <App />
       </DataContext>
     </AuthContext>
+    
   </BrowserRouter>
   ,
 )
