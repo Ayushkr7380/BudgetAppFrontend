4a95c125a4c0234dc582c5db3e3bef3f38c7cfea
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-08 13:38:51 +0530

    login and signup component added

--- src/CustomRoutes/BudgetRouters.jsx
+++ src/CustomRoutes/BudgetRouters.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Components/Home/Home';
 import Profile from '../Components/Profile/Profile';
+import Login from '../Components/Login/Login';
+import Signup from '../Components/Signup/Signup';
 
 function BudgetRouters(){
     return(
         <>
             <Routes>
+                <Route path='/auth/signup' element={<Signup/>}></Route>
+                <Route path='/auth/login' element={<Login/>}></Route>
+
                 <Route path='/' element={<Home/>}></Route>
                 <Route path='/profile' element={<Profile/>}></Route>
             </Routes>
