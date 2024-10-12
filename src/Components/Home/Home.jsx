c91a450fda84830c623917e0335dd7c6344d808d
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-13 00:50:51 +0530

    working on homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react"
-
-// import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { CreateDataContext } from "../../Context/DataContext/CreateDataContext";
 const Home = () => {
-  const [ inputFieldLength , setInputFieldLength ] = useState([1]);
-
-  const addInput = ()=>{
-    setInputFieldLength([inputFieldLength.length + 1])
-  }
-  console.log(inputFieldLength)
+  const context = useContext(CreateDataContext);
+  const { handleChange , submitExpenditureDetails , expenditureDetails } = context;
   return (
-    <div className="flex justify-center">
-        {
-          inputFieldLength
-        }
-        <div className="flex ">
-        
-          
+    <div className="flex items-center flex-col border-2 border-white min-h-48 p-4">
+      <h2>Write your daily expenditure here</h2>
+      <div className="flex flex-col m-2">                        
+                <input type="text"
+                    className="border-2 border-black p-2 text-black"
+                    placeholder="Expenditure Name"
+                    onChange={handleChange} 
+                    name="ExpenditureName" 
+                    value={expenditureDetails.ExpenditureName}
+                  />
+                <input type="text"
+                  className="border-2 border-black p-2 text-black" 
+                  placeholder="Expenditure Amount" 
+                  onChange={handleChange} 
+                  name="ExpenditureAmount" 
+                  value={expenditureDetails.ExpenditureAmount}
+                 />   
+                <input type="date" 
+                  onChange={handleChange} 
+                  name="ExpenditureDate"
+                  className="text-black"
+                  value={expenditureDetails.ExpenditureDate}
+                 />        
         </div>
-        <div>
-          <button onClick={()=>addInput()}>Add</button>
+        <div >
+          <button className="border-white border-2 p-2 rounded-md" onClick={submitExpenditureDetails}>Add</button>
         </div>
     </div>
   )
--- src/Context/DataContext/DataContext.jsx
+++ src/Context/DataContext/DataContext.jsx
@@ -1,9 +1,23 @@
 import { CreateDataContext } from "./CreateDataContext";
-
+import { useState } from "react";
 function DataContext(props){
+    const [ expenditureDetails , setExpenditureDetails] = useState({ExpenditureName:'',ExpenditureAmount:'',ExpenditureDate:''});
+
+  const handleChange =(e)=>{
+      const { name , value } = e.target;
+      setExpenditureDetails({
+        ...expenditureDetails,
+        [name]:value
+      });
+  }
+  // console.log(expenditureDetails);
+  const submitExpenditureDetails = () =>{
+    console.log(expenditureDetails); 
+    setExpenditureDetails({ExpenditureName:'',ExpenditureAmount:'',ExpenditureDate:''})  
+  }
     return(
         <>
-            <CreateDataContext.Provider value={{}}>
+            <CreateDataContext.Provider value={{handleChange,submitExpenditureDetails,expenditureDetails}}>
                 {props.children}
             </CreateDataContext.Provider>
         </>
