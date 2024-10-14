b1359e276f0be8e3cd0ab409ddeac7ecd8da36d9
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-14 23:22:26 +0530

    signup page added

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,6 +1,34 @@
+import{ Link } from "react-router-dom";
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="text-white flex justify-center ">
+      <div className="flex flex-col border-2 border-white py-2 px-6 rounded-md">
+        <h1 className="font-bold text-2xl text-center my-3">SignUp Page</h1>
+        <input
+         type="text"
+         className="text-white px-2 py-1 m-2 rounded-md"
+         name="name"
+         placeholder="Name"
+         />
+         <input
+         type="text"
+         className="text-white px-2 py-1 m-2 rounded-md"
+         name="email"
+         placeholder="Email"
+         />
+         <input
+         type="password"
+         className="text-white px-2 py-1 m-2 rounded-md"
+         name="password"
+         placeholder="Password"
+         />
+
+         <div className="grid grid-cols-2 items-center text-center mt-3">
+          <button className="border-white border-2  rounded-md font-semibold">Signup</button>
+          <Link to='/auth/login' className="underline" >Login</Link>
+         </div>
+      </div>
+    </div>
   )
 }
 
