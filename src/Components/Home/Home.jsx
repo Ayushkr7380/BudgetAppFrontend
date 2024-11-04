9bd81576f55e4b8a79c66367ffa5a54215c58765
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-11-04 21:21:16 +0530

    css updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { CreateDataContext } from "../../Context/DataContext/CreateDataContext";
 import { CreateAuthContext } from "../../Context/AuthContext/CreateAuthContext";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const Home = () => {
   const context = useContext(CreateDataContext);
@@ -52,12 +54,12 @@ const Home = () => {
 
   return (
     <div className="flex items-center flex-col">
-      <h2>Write your daily expenditure here</h2>
+      <h2 className="text-2xl font-semibold underline">Write your daily expenditure here</h2>
       
-      <div className="flex flex-col m-2 border-2 border-white min-h-48 p-4">                        
+      <div className="flex flex-col m-2 border-2 border-white min-h-40 justify-center px-2 py-1 rounded-md ">                        
         <input
           type="text"
-          className="border-2 border-black p-2 text-black"
+          className="border-2 border-black px-2 py-1 my-1 outline-none text-black rounded-md"
           placeholder="Expenditure Name"
           onChange={handleChange}
           name="ExpenditureName"
@@ -65,7 +67,7 @@ const Home = () => {
         />
         <input
           type="text"
-          className="border-2 border-black p-2 text-black"
+          className="border-2 border-black outline-none px-2 py-1 my-1 text-black rounded-md"
           placeholder="Expenditure Amount"
           onChange={handleChange}
           name="ExpenditureAmount"
@@ -75,21 +77,25 @@ const Home = () => {
           type="date"
           onChange={handleChange}
           name="ExpenditureDate"
-          className="text-black"
+          className="text-black rounded-md my-1 px-2"
           value={expenditureDetails.ExpenditureDate}
         />        
       </div>
       
       <button
-        className="border-white border-2 p-2 rounded-md"
+        className="border-white border-2 px-3 py-1 rounded-md font-bold hover:bg-white hover:text-black"
         onClick={submitExpenditureDetails}
       >
         Add
       </button>
       
-      <div className="grid grid-cols-4 mt-4">
+      <div className="grid md:grid-cols-4 mt-4 grid-cols-2">
         {Object.keys(groupedByDate).map((date) => (
           <div key={date} className="mb-4 border p-3 text-black m-2 bg-slate-300 rounded-md">
+            <div className="flex justify-end md:text-xl mb-3">
+              <p className="text-green-600 mx-1 px-1"><FaEdit /></p>
+              <p className="text-red-600  mx-1 px-1"><MdDelete /></p>
+            </div>
             <h3 className="font-bold mb-2 text-black">{date}</h3>
             <ul>
               {groupedByDate[date].items.map((item, index) => (
